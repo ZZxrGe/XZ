01067337365ff02d4e03dbc416a6af176f553f2e
ZZxrGe <ddr8828@gmail.com>
2025-11-28 13:17:35 +0300

    Update #4.py

--- #4.py
+++ #4.py
@@ -291,6 +291,14 @@ print(f"Результат: {result}")
 #3
 number_1 = 10
 number_2 = 100
+user_number = int(input("Введите своё число: "))
+
+if number_1 < user_number < number_2:
+    print(True)
+else:
+    print(False)
+              
+
 
 
 
